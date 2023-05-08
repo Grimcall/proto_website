0c511993eb47402c5a172df8cccbbe1efaa06123
Grimcall <mccoycross@gmail.com>
2023-05-08 17:52:51 -0400

    AboutMe and Banner translations done.

--- flask_backend/translations.py
+++ flask_backend/translations.py
@@ -1,25 +1,39 @@
 TRANSLATIONS = {
     'AboutMe':{
         "en":{
-            "greeting": "hello",
-            "goodbye": "goodbye",
+            "h1_text":"Who am I?",
+            "p1": "As stated above, I'm Diego Parra (nice to meet you) and I'm also a systems engineer.",
+            "h2_text": "That means I solve problems.",
+            "p2":"Not problems such as 'what is beauty?', because that would fall within the purview of your conundrums of philosophy...",
+            "h2_text_2": "I solve practical problems.",
+            "p3": "Dealing with software these days can be daunting, complicated and even scary...  especially when you're dealing with newer, more complex systems! However, that's where I come in.\nI have the knowledge, skills, and experience to help you navigate these challenges and come out on top. I'm not satisfied until I've found a solution that meets your needs and exceeds your expectations: whether it's developing software, troubleshooting systems, or finding ways to optimize your infrastructure, I'm here to help.\nAt the end of the day, that's what being an engineer is all about: ",
+            "h2_text_3": "Solving practical problems, one piece at a time.",
         },
 
         "es":{
-            "greeting": "hola",
-            "description": "chao",
+            "h1_text":"¿Quién soy yo?",
+            "p1": "Como dije anteriormente, soy Diego Parra (encantado de conocerle) y soy ingeniero de sistemas.",
+            "h2_text": "Eso significa que soluciono problemas.",
+            "p2":"No problemas como '¿qué es la belleza?', porque eso caería dentro del ámbito de tu perspectiva y filosofía...",
+            "h2_text_2": "Soluciono problemas prácticos.",
+            "p3": "Lidiar con software en esta época puede ser engorroso, complicado y difícil... ¡especialmente cuando tratas con sistemas más nuevos y complejos! Sin embargo, ahí es donde entro yo.\nTengo el conocimiento, las habilidades y la experiencia para ayudarle a superar estos desafíos y encaminarlo a la victoria. No estoy satisfecho hasta que logre encontrar una solución que satisfaga sus necesidades y supere sus expectativas: ya sea desarrollando software, solucionando problemas de sistemas o buscando formas de optimizar su infraestructura, estoy aquí para ayudarle.\nAl final del día, de eso se trata ser ingeniero: ",
+            "h2_text_3": "Solucionar problemas prácticos, una pieza a la vez.",
         },
     },
 
     'Banner':{
         "en":{
-            "greeting": "hello",
-            "goodbye": "goodbye",
+            "welcome":"Welcome to my website!",
+            "h1_greeting": "Hi!\nI'm Diego Parra,\nSystems Engineer.",
+            "p": "I'm a systems engineer graduate with a passion for software, web & mobile applications development and UI/UX design utilizing systems-oriented paradigms.\nMy main focus is on backend development, with my strongest programming language being Python.",
+            "lets-connect": "Let's talk.",
         },
 
         "es":{
-            "greeting": "hola",
-            "description": "chao",
+            "welcome":"¡Bienvenido a mi página!",
+            "h1_greeting": "¡Hola!\nSoy Diego Parra,\Ingeniero de Sistemas.",
+            "p": "Soy recién graduado en ingeniería de sistemas y me apasionan el desarrollo de software, desarollo de aplicaciones web y móviles al igual que el diseño de interfaces gráficas utilizando paradigmas orientados a sistemas.\nMi enfoque principal es el desarrollo de back-end, siendo Python mi lenguaje de programación más sólido.",
+            "lets-connect": "Conversémos.",
         },
     },
 
